025eda38ccd423b7e6c7706eaf8fc02f804f7d9c
WaiLinnAung16 <waia00198@gmail.com>
2023-07-11 10:37:51 +0630

    add loading and btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Loading from "./components/Loading/Loading";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, [3000]);
+  }, []);
   return (
-    <div className=" font-Montserrat">
-      <Home />
-    </div>
+    <div className=" font-Montserrat">{loading ? <Loading /> : <Home />}</div>
   );
 };
 
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,11 +4,10 @@ const Loading = () => {
   const words = ["P", "O", "R", "T", "F", "O", "L", "I", "O"];
   return (
     <div className=" flex justify-center items-center h-screen bg-white">
-      <div className="h-20 px-5 overflow-hidden flex items-baseline page">
+      <div className="h-20 w-[360px] overflow-hidden flex items-baseline page">
         {words.map((w) => {
           return <h1 className="text">{w}</h1>;
         })}
-        <span className="w-3 h-3 bg-orange-500 text"></span>
       </div>
     </div>
   );

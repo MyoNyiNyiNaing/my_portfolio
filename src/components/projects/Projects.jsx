dd413cfd649bb36fc7a907a55bc3e05bc64d0347
Min1870 <minkhantzaw.peti@gmail.com>
2023-07-12 11:50:37 +0630

    update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,8 +4,8 @@ import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
   return (
-    <div className="">
-      <div className=" pt-[80px] max-w-[1215px] px-5  mx-auto flex items-center justify-between ">
+    <div id="projects" className="">
+      <div className="max-w-[1215px] px-5  mx-auto flex items-center justify-between ">
         <div className="">
           <div>
             <h6 className=" text-base font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 via-red-500 to-yellow-500 inline-block uppercase tracking-[4px]">
@@ -29,7 +29,7 @@ const Projects = () => {
           <div className="button_down"></div>
         </div>
       </div>
-      <div className="grid grid-col-1 items-center justify-center md:grid-cols-3 gap-x-5 max-w-[1215px] px-5 mx-auto pt-[65px] pb-[80px] gap-10">
+      <div className="grid grid-col-1 items-center justify-center md:grid-cols-3 gap-x-5 max-w-[1215px] px-5 mx-auto pt-[65px] gap-10">
         {projects.map((project) => (
           <div className="group">
             <ProjectsCard key={project.id} {...project} />

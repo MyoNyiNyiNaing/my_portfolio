3a74ea8d373791ec968468c81fdd3118d5c7582a
WaiLinnAung16 <waia00198@gmail.com>
2023-07-13 16:23:28 +0630

    add mobile theme btn

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { services } from "./services";
 import ServicesCard from "./ServicesCard";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import Aos from "aos";
 
 const Services = () => {
   Aos.init();
 
   return (
-    <div id="services"  className="max-w-[1215px] px-5 md:mx-auto flex flex-col justify-center">
-      <div className=" text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="100">
+    <div
+      id="services"
+      className="max-w-[1215px] px-5 md:mx-auto flex flex-col justify-center"
+    >
+      <div
+        className=" text-center"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        data-aos-once="true"
+        data-aos-delay="100"
+      >
         <div className=" inline-block transform translate-x-0 translate-y-0 translate-z-0 scale-x-100 scale-y-100 scale-z-100 rotate-x-0 rotate-y-0 rotate-z-0 skew-x-0 skew-y-0 transform-style-preserve-3d">
           <h6 className=" text-transparent bg-clip-text leading-[100%] bg-gradient-to-r from-purple-600 via-red-500 to-yellow-500 inline-block font-bold text-base uppercase tracking-[4px]">
             Services
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
@@ -11,15 +11,42 @@ import Contact from "../components/Contact";
 import ThemeBtn from "../components/Theme/ThemeBtn";
 
 const Home = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const iconToggle = () => {
+    return setDarkMode((darkMode) => !darkMode);
+  };
+
+  const ThemeCheck = () => {
+    if (localStorage.getItem("theme") === "dark") {
+      document.documentElement.classList.add("dark");
+      setDarkMode(true);
+      return;
+    }
+    setDarkMode(false);
+  };
+
+  const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      iconToggle();
+      return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+  };
+
+  useEffect(() => {
+    ThemeCheck();
+  }, []);
   return (
     <>
-
-      <Header />
-      <main className=" relative dark:bg-black">
-        <ThemeBtn />
+      <Header darkMode={darkMode} themeSwitch={themeSwitch} />
+      <main className=" relative dark:bg-[#150112] dark:text-white">
+        <ThemeBtn darkMode={darkMode} themeSwitch={themeSwitch} />
         <section className="">
           <Hero />
-
         </section>
         <section className=" py-[80px]">
           <Services />

8ee169271ca3f1034c65ae832540c6436f295104
WaiLinnAung16 <waia00198@gmail.com>
2023-07-13 11:05:24 +0630

    add darkMode btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Hero from '../components/Hero'
-import Services from '../components/services/Services.jsx'
-import Projects from '../components/projects/Projects.jsx'
-import About from '../components/About'
-import Testimonials from '../components/Testimonials'
-import Faq from '../components/Faq'
-import Contact from '../components/Contact'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Services from "../components/services/Services.jsx";
+import Projects from "../components/projects/Projects.jsx";
+import About from "../components/About";
+import Testimonials from "../components/Testimonials";
+import Faq from "../components/Faq";
+import Contact from "../components/Contact";
+import ThemeBtn from "../components/Theme/ThemeBtn";
 
 const Home = () => {
   return (
     <>
-    
-    <Header/>
-    <main>
-        <section className=''>
-                <Hero/>
+      <Header />
+      <main className=" relative dark:bg-black">
+        <ThemeBtn />
+        <section className="">
+          <Hero />
         </section>
-        <section className=' py-[80px]'>
-                <Services/>
+        <section className=" py-[80px]">
+          <Services />
         </section>
-        <section className=' py-[80px]'>
-                <Projects/>
+        <section className=" py-[80px]">
+          <Projects />
         </section>
-        <section className=' py-[80px]'>
-                <About/>
+        <section className=" py-[80px]">
+          <About />
         </section>
-        <section className=' py-[80px] pb-[160px]'>
-                <Contact/>
+        <section className=" py-[80px] pb-[160px]">
+          <Contact />
         </section>
-    </main>
-    <Footer/>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
